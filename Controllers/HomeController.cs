bea66300d94be075cc4d1e9b7a63fd93b36791eb
Esraa Soliman <ee341110@gmail.com>
2025-10-14 21:42:23 +0300

    create main pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,34 @@ namespace Gym.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // GET: /Home/Index
         public IActionResult Index()
         {
+            ViewData["Title"] = "Home";
             return View();
         }
 
-        public IActionResult Privacy()
+        // GET: /Home/About
+        public IActionResult About()
         {
+            ViewData["Title"] = "About Us";
+            ViewBag.Message = "Learn more about our gym and what we offer!";
             return View();
         }
 
+        // GET: /Home/Contact
+        public IActionResult Contact()
+        {
+            ViewData["Title"] = "Contact Us";
+            ViewBag.Message = "We’d love to hear from you!";
+            return View();
+        }
+
+        // Optional: Error Page
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
